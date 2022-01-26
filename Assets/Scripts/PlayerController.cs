8f614708e8dce663268c377947d89411427d0aff
Nikhil Bansal <91827364n@gmail.com>
2022-01-26 22:37:08 +0530

    Polished Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundMask;
+
     public float speedOfCharacter;
     public float jumpForce;
     public float crouchHeight;
+    public float groundCheckRadius;
 
     private Animator _animator;
     private Rigidbody2D _rigidbody;
@@ -15,9 +18,11 @@ public class PlayerController : MonoBehaviour
     private float horizontalMove; //to take input for the horizontal movement of the player
     private float verticalMove; //to take input for the vertical movement of the player
     private float normalColHeight; //box collider's heaight
-    private float normalCOLOffset;
+    private float normalCOLOffset; //box collider's offset
     private bool isCrouching; //bool variable for crouching of the character and its animation
+    private bool hasLanded;
     private bool canJump;
+    private bool isGrounded;
 
 
     private void Start()
@@ -28,6 +33,7 @@ public class PlayerController : MonoBehaviour
 
         normalColHeight = _capsuleCollider.size.y;
         normalCOLOffset = _capsuleCollider.offset.y;
+        canJump = true;
     }
 
     private void Update()
@@ -42,19 +48,46 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        CheckGround();
         MoveCharacter();
+    }
+
+    private void CheckGround()
+    {
+        bool wasGrounded = isGrounded;
+        isGrounded = false;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundCheckRadius, groundMask);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                isGrounded = true;
+                if (!wasGrounded)
+                {
+                    hasLanded = true;
+                    canJump = true;
+                }
+            }
+        }
+
+        if (!isGrounded)
+        {
+            hasLanded = false;
+        }
     }
 
     public void MoveCharacter()
     {
         Vector3 pos = transform.position;
         //Horizontal Movement
-        pos.x += horizontalMove * speedOfCharacter * Time.fixedDeltaTime;
+        pos.x += horizontalMove * speedOfCharacter * Time.deltaTime;
         //Vertical Movement
-        if(verticalMove > 0 && canJump)
+        if(verticalMove > 0 && canJump && !isCrouching)
         {
             _rigidbody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
             canJump = false;
+            hasLanded = false;
         }
         transform.position = pos;
 
@@ -82,6 +115,7 @@ public class PlayerController : MonoBehaviour
         _animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
         _animator.SetBool("Jump", (verticalMove > 0 && canJump) ? true : false);
         _animator.SetBool("Crouch", isCrouching);
+        _animator.SetBool("Land", hasLanded);
     }
 
     public void Flip()
@@ -102,7 +136,7 @@ public class PlayerController : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Ground"))
         {
-            canJump = true;
+            hasLanded = true;
         }
     }
 }
